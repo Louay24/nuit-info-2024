63ccf0e36842f34055aba30104a717264243647f
louay24 <loueyzouaoui@takiacademyteam.com>
2024-12-06 01:27:23 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { NotFound, Game } from "./pages";
+import { Game } from "./pages";
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/game" element={<Game />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
+  return <Game />;
 };
 
 export default App;
